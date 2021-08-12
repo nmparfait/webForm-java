3519df655272cca04be119e8a0bb12caa4765baf
nmparfait <mbamu.developer@gmail.com>
2021-08-12 12:05:46 -0400

    showing the added users on the screen

--- App2021/src/edu/br/infnet/model/controller/AlunoController.java
+++ App2021/src/edu/br/infnet/model/controller/AlunoController.java
@@ -52,15 +52,33 @@ public class AlunoController extends HttpServlet {
 				"	</head>\r\n" + 
 				"	<body>\r\n" + 
 				"		<div class=\"container\">\r\n" + 
-				"			<h3>Aluno "+aluno.getNome() +" cadastro com sucesso!!!!</h3>\r\n" + 
-				"			<h3>Quantidade de alunos existentes: "+alunos.size() +" !!</h3>\r\n" + 
-				
-				"			<form action=\"home.html\" method=\"get\">\r\n" + 
-				"				<button type=\"submit\" class=\"btn btn-default\">voltar</button>\r\n" + 
-				"			</form>\r\n" + 
+				"			<a href='home.html'>Voltar</a>\r\n" + 
+				"			<h2>Aluno "+aluno.getNome() +" cadastro com sucesso!!!!</h2>\r\n" + 
+				"			<h3>Quantidade de alunos existentes: "+ alunos.size() +" </h3>\r\n" +  
 				"		</div>	\r\n" + 
 				"	</body>\r\n" + 
 				"</html>");
+		
+		
+		//list of users printed on the screen
+		for(Aluno a: alunos) {
+			out.println(" <!DOCTYPE html>\r\n" + 
+					"<html>\r\n" + 
+					"	<head>\r\n" + 
+					"		<meta charset=\"ISO-8859-1\">\r\n" + 
+					"		<link rel=\"stylesheet\" href=\"https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css\">\r\n" + 
+					"		<title>App_2021: Confirma</title>\r\n" + 
+					"	</head>\r\n" + 
+					"	<body>\r\n" + 
+					"		<div class=\"container\">\r\n" + 
+					"       <hr>\r\n"+
+					"			<h3>Aluno: " + aluno.getNome() +" </h3>\r\n" +  
+					"		</div>	\r\n" + 
+					"	</body>\r\n" + 
+					"</html>");
+					
+					
+		}
 		
 		
 		//request.getRequestDispatcher("confirmacao.html").forward(request, response);
